1f61f172b5c98d243a0f148e7cf449f74f745559
Trần Tiến Phúc <112532197+tttpppppp@users.noreply.github.com>
2025-08-02 14:07:59 +0700

    simple express app

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+const port = process.env.SERVER_PORT || 3000;
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
