d8b6a91e2e7aaf6d630c82df330d159f85e1badd
Trần Tiến Phúc <112532197+tttpppppp@users.noreply.github.com>
2025-08-02 23:41:57 +0700

    connect db

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { Routes } from "core/interface";
 import express from "express";
-
+import mongoose from "mongoose";
 class App {
   public app: express.Application;
   public port: string | number;
@@ -9,6 +9,7 @@ class App {
     this.app = express();
     this.port = process.env.SERVER_PORT || 3000;
     this.initializeRoutes(routes);
+    this.connectToDatabase();
   }
 
   private initializeRoutes(routes: Routes[]) {
@@ -21,6 +22,21 @@ class App {
     this.app.listen(this.port, () => {
       console.log(`App listening on the port ${this.port}`);
     });
+  }
+  private async connectToDatabase() {
+    const dbUrl = process.env.DATABASE_URL;
+    if (!dbUrl) {
+      console.error("DATABASE_URL is not defined.");
+      return;
+    }
+    try {
+      await mongoose.connect(dbUrl, {
+        dbName: "nodejs_app",
+      });
+      console.log("Connected to the database successfully");
+    } catch (error) {
+      console.error("Database connection error:", error);
+    }
   }
 }
 
